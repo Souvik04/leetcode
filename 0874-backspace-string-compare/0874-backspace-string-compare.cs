39dcacb69add612ea81574cdd63562e913c4f756
souvikg04 <148306526+souvikg04@users.noreply.github.com>
2023-10-22 16:52:21 +0530

    Time: 60 ms (85.77%), Space: 38.2 MB (73.67%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cs
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cs
@@ -1,34 +1,51 @@
 public class Solution {
     public bool BackspaceCompare(string s, string t) {
-        string sNew = GetString(s);
-        string tNew= GetString(t);
+        int sCount = 0;
+        int tCount = 0;
+        int i = s.Length - 1;
+        int j = t.Length - 1;
 
-        if(sNew.Length == tNew.Length){
-            return sNew.Equals(tNew);
-        }
-        
-        return false;
-    }
-
-    private string GetString(string str){
-        Stack<char> stack = new Stack<char>();
-        for(int i = 0; i < str.Length; i++){
-            if(str[i] != '#'){
-                stack.Push(str[i]);
-            }
-            else{
-                if(stack.Count > 0){
-                    stack.Pop();
+        while(i >= 0 || j >= 0){
+            while( i >= 0){
+                if(s[i] == '#'){
+                    sCount++;
+                    i--;
+                }
+                else if(sCount > 0){
+                    sCount--;
+                    i--;
+                }
+                else{
+                    break;
                 }
             }
-        }        
 
-        StringBuilder sb = new StringBuilder();
+            while( j >= 0){
+                if(t[j] == '#'){
+                    tCount++;
+                    j--;
+                }
+                else if(tCount > 0){
+                    tCount--;
+                    j--;
+                }
+                else{
+                    break;
+                }
+            } 
 
-        while(stack.Count > 0){
-            sb.Append(stack.Pop());
+            if(i >= 0 && j >= 0 && s[i] != t[j]){
+                return false;
+            }
+
+            if((i >= 0) != (j >= 0)){
+                return false;
+            }
+
+            i--;
+            j--;
         }
 
-        return sb.ToString();        
+        return true;
     }
 }

1e72b26a11bfbba2c7206b556d03abf73d65912a
souvikg04 <148306526+souvikg04@users.noreply.github.com>
2023-10-31 16:55:37 +0530

    Time: 115 ms (100%), Space: 46.1 MB (100%) - LeetHub

--- 1106-escape-a-large-maze/1106-escape-a-large-maze.cs
+++ 1106-escape-a-large-maze/1106-escape-a-large-maze.cs
@@ -1,68 +1,55 @@
-public class Solution {
-    public bool IsEscapePossible(int[][] blocked, int[] source, int[] target) {
-        int maxBlocks = blocked.Length;
-        HashSet<long> visitedSource = new HashSet<long>();
-        HashSet<long> visitedTarget = new HashSet<long>();
-
-        foreach (var b in blocked) {
-            if (Math.Abs(b[0] - source[0]) + Math.Abs(b[1] - source[1]) < 400){
-                visitedSource.Add(((long)b[0] << 32) + b[1]);
+public class Solution{
+	public bool IsEscapePossible(int[][] blocked, int[] source, int[] target){
+		HashSet<long> visitedSource = new HashSet<long>();
+		HashSet<long> visitedTarget = new HashSet<long>();
+		
+		foreach(var b in blocked){
+			if(Math.Abs(b[0] - source[0]) + Math.Abs(b[1] - source[1]) < 400){
+				visitedSource.Add(((long)b[0] << 32) + b[1]);
             }
-                
-            if (Math.Abs(b[0] - target[0]) + Math.Abs(b[1] - target[1]) < 400){
-                visitedTarget.Add(((long)b[0] << 32) + b[1]);
+
+	        if(Math.Abs(b[0] - target[0]) + Math.Abs(b[1] - target[1]) < 400){
+		        visitedTarget.Add(((long)b[0] << 32) + b[1]);
             }
         }
 
-        return BFS(visitedSource, source, target, maxBlocks) && BFS(visitedTarget, target, source, maxBlocks);
+        return (BFS(visitedSource, source, target, blocked.Length) && BFS(visitedTarget, target, source, blocked.Length));
     }
 
-    private bool BFS(HashSet<long> visited, int[] source, int[] target, int maxBlocks) {
+    private bool BFS(HashSet<long> visited, int[] source, int[] target, int maxBlocks){
         int[] dr = new int[]{1, 0, -1, 0};
         int[] dc = new int[]{0, 1, 0, -1};
 
-        Queue<int[]> queue = new Queue<int[]>();
-        queue.Enqueue(source);
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+        queue.Enqueue((source[0], source[1]));
 
-        while (queue.Count > 0 && queue.Count <= maxBlocks) {
-            Queue<int[]> newQueue = new Queue<int[]>();
+        while(queue.Count > 0 && queue.Count <= maxBlocks){
             int count = queue.Count;
 
             for(int i = 0; i < count; i++){
                 var cur = queue.Dequeue();
 
                 for(int d = 0; d < 4; d++){
-                    int x = cur[0] + dr[d];
-                    int y = cur[1] + dc[d];
+                    int x = dr[d] + cur.Item1;
+                    int y = dc[d] + cur.Item2;
 
-                    if (x < 0 || x > 999999 || y < 0 || y > 999999){
+                    if(x < 0 || x > 999999 || y < 0 || y > 999999){
                         continue;
                     }
-                     
-                    if (x == target[0] && y == target[1]){
+
+                    if(x == target[0] && y == target[1]){
                         return true;
-                    } 
+                    }
 
                     long key = ((long)x << 32) + y;
-                    if (visited.Add(key)){
-                        queue.Enqueue(new int[] { x, y });
+                    
+                    if(visited.Add(key)){
+                        queue.Enqueue((x, y));
                     }
-                }                
+                }
             }
         }
 
         return queue.Count > 0;
     }
 }
-
-
-
-
-/*
-. # . . .
-# . . . .
-. . . . .
-. . . . .
-. . . . .
-
-*/

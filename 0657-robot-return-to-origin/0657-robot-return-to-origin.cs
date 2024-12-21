f155e885bac57022c7bc24904ccbcf33d7d1822a
Souvik Ghosh <g.souvik04@gmail.com>
2024-12-21 19:15:47 +0530

    Time: 3 ms (97.84%), Space: 43.6 MB (26.49%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        
+        foreach(char c in moves){
+            if(c == 'U'){
+                y++;
+            }
+            else if(c == 'D'){
+                y--;
+            }
+            else if(c == 'R'){
+                x++;
+            }
+            else if(c == 'L'){
+                x--;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+}

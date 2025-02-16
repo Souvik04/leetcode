fb14a5ce3bd94557b81fc8cf25314615c7419549
Souvik Ghosh <g.souvik04@gmail.com>
2025-02-16 18:04:14 +0530

    Time: 2 ms (70.03%), Space: 54.2 MB (65.22%) - LeetHub

--- 0155-min-stack/0155-min-stack.cs
+++ 0155-min-stack/0155-min-stack.cs
@@ -1,78 +1,30 @@
 public class MinStack {
-    Stack<(int val, int min)> _stack = null;
+    Stack<(int val, int min)> stack = null;
     public MinStack() {
-        _stack = new Stack<(int val, int min)>();
+        stack = new Stack<(int, int)>();
     }
     
     public void Push(int val) {
-        if(_stack.Count == 0){
-            _stack.Push((val, val));
+        if(stack.Count == 0){
+            stack.Push((val, val));
         }
         else{
-            _stack.Push((val, Math.Min(_stack.Peek().min, val)));
+            stack.Push((val, Math.Min(val, stack.Peek().min)));
         }
     }
     
     public void Pop() {
-        _stack.Pop();
+        stack.Pop();
     }
     
     public int Top() {
-        return _stack.Peek().val;
+        return stack.Peek().val;
     }
     
     public int GetMin() {
-        return _stack.Peek().min;
+        return stack.Peek().min;
     }
 }
-
-
-/*
-
-// Linked list based
-
-public class MinStack {
-    private Node _head;
-
-    public MinStack() {
-
-    }
-    
-    public void Push(int val) {
-        if(_head == null){
-            _head = new Node(val, val, null);
-        }
-        else{
-            _head = new Node(val, Math.Min(val, _head.min), _head);
-        }
-    }
-    
-    public void Pop() {
-        _head = _head.next;
-    }
-    
-    public int Top() {
-        return _head.val;
-    }
-    
-    public int GetMin() {
-        return _head.min;
-    }
-
-    private class Node{
-        public int val;
-        public int min;
-        public Node next;
-
-        public Node(int val, int min, Node next){
-            this.val = val;
-            this.min = min;
-            this.next = next;
-        }
-    }
-}
-
-*/
 
 /**
  * Your MinStack object will be instantiated and called as such:
